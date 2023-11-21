72edfc197463b0605aa339bf833e3f2a534e41e8
Descotto <davidescotto@hotmail.com>
2023-11-20 19:47:05 -0800

    damage particles

--- level.py
+++ level.py
@@ -3,7 +3,7 @@ from settings import *
 from tile import Tile
 from player import Player
 from support import *
-from random import choice
+from random import choice, randint
 from weapon import Weapon
 from ui import UI
 from controls import Control
@@ -117,7 +117,9 @@ class Level:
                     for target_sprite in collision_sprites:
                         if target_sprite.sprite_type == 'grass':
                             pos = target_sprite.rect.center
-                            self.animation_player.create_grass_particles(pos,[self.visible_sprites])
+                            offset = pygame.math.Vector2(0,75)
+                            for leaf in range(randint(3,6)):
+                                self.animation_player.create_grass_particles(pos - offset,[self.visible_sprites])
                             target_sprite.kill()
                         else:
                             target_sprite.get_damage(self.player,attack_sprite.sprite_type)
@@ -127,6 +129,7 @@ class Level:
             self.player.health -= amount
             self.player.vulnerable = False
             self.player.hurt_time = pygame.time.get_ticks()
+            self.animation_player.create_particles(attack_type,self.player.rect.center,[self.visible_sprites])
 
     def run(self):
         # update and draw game objects
